f48afd3907f4aff4d09b73ba14df14c32e088171
Lucas LEMOY <lucaslemoy@gmail.com>
2020-01-22 11:37:10 +0100

    New with return a list

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask,jsonify
 app = Flask(__name__)
+PRODUCTS = [{ 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' }]
 
-@app.route('/')
+@app.route('/api/v1/products')
 def hello():
-    return "Hello Lucassss!"
+    return jsonify(PRODUCTS)
